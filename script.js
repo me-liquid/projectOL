96f4091b981d8e048ac8a73d856829ae683f364f
me-liquid <81876757+me-liquid@users.noreply.github.com>
2025-01-25 23:46:32 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Function to show a success message when the "Send Message" button is clicked
+function submitMessage(event) {
+    event.preventDefault(); // Prevents the form from submitting normally
+    alert('Your message has been sent successfully!');
+  }
+  
